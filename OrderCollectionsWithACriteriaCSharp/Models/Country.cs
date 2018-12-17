6964dda0dea4fddc71c84e22879056d61f80eca4
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-17 18:37:26 -0200

    Adding video contents

---
+++ OrderCollectionsWithACriteriaCSharp/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderCollectionsWithACriteriaCSharp.Models
+{
+    public class Country
+    {
+        public static IEnumerable<Country> Countries = new[]
+        {
+            new Country { Id = Guid.NewGuid(), Name = "Canada" },
+            new Country { Id = Guid.NewGuid(), Name = "Brasil" },
+            new Country { Id = Guid.NewGuid(), Name = "Australia" },
+        };
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ OrderCollectionsWithACriteriaCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using OrderCollectionsWithACriteriaCSharp.Models;
+using System;
+using System.Linq;
+
+namespace OrderCollectionsWithACriteriaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countries = Country.Countries;
+            var orderedCountries = countries.OrderBy(x => x.Name);
+            foreach (var country in orderedCountries)
+            {
+                Console.WriteLine(country.Name);
+            }
+            // Wait for a key to be pressed while executing
+            Console.ReadKey();
+        }
+    }
+}
